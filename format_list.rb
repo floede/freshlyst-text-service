f85603fdef60e5d8eec7857124a35c35625922f2
Janus Hasseriis <janus@hasseriis.net>
2019-10-13 23:50:53 +0200

    First working version

---
+++ format_list.rb
@@ -0,0 +1,35 @@
+require 'sinatra'
+require 'sinatra/reloader'
+
+def formatted_list (list)
+    list&.gsub(/\([^()]*\)/, '')
+end
+
+def format_rioc_list (list)
+    list&.gsub!(/\([^()]*\)/, '')
+    list&.gsub!(/^[0-9,.,\s]+/, '')
+end
+
+def format_lfhq_list (list)
+    list&.gsub!(/\([^()]*\)/, '')
+    list&.gsub!(/^[0-9,.,\s]+/, '')
+    list&.gsub!(/[-]+/, ' - ')
+end
+
+get '/' do
+    org_list = params["list"]
+    formatted_list = formatted_list(org_list)
+    erb :index, :locals => {:formatted_list => formatted_list}
+end
+
+get '/rioc' do
+    org_list = params["list"]
+    formatted_list = format_rioc_list(org_list)
+    erb :index, :locals => {:formatted_list => formatted_list}
+end
+
+get '/lfhq' do
+    org_list = params["list"]
+    formatted_list = format_lfhq_list(org_list)
+    erb :index, :locals => {:formatted_list => formatted_list}
+end
